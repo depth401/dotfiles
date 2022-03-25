445e2e623500c17f5613038b29abf61db204e5e5
depth401 <mental_pg@protonmail.com>
2022-03-26 00:31:40 +0900

    [zsh] Improve history

--- dot/zsh/.zsh/basic.zsh
+++ dot/zsh/.zsh/basic.zsh
@@ -1,11 +1,17 @@
 ## 履歴保存管理
-HISTFILE=$ZDOTDIR/.zsh-history
 HISTSIZE=100000
 SAVEHIST=1000000
 
 ## 他のzshと履歴を共有
 setopt inc_append_history
 setopt share_history
+
+## 重複排除
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
+
+setopt hist_no_store
+setopt hist_reduce_blanks
 
 ## パスを直接入力してもcdする
 setopt AUTO_CD
